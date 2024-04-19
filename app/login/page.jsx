66f63cd3e78f98b1af5ee530f020a5f6846aa8a0
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2024-04-19 15:45:24 -0700

    updated login page to fit form into blue section

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,67 +1,59 @@
-
- import Layout from "../components/Layout/Layout";
-import Link from 'next/link';
-import loginStyles from './login.module.css';
+import Layout from "../components/Layout/Layout";
+import Link from "next/link";
+import loginStyles from "./login.module.css";
 import Button from "../components/Button/Button";
-import Image from 'next/image';
-
+import Image from "next/image";
 
 function LoginPage() {
-  const handleChange =(event)=>{
+  const handleChange = (event) => {
     console.log(event.target.value);
-  }
+  };
 
-      
   return (
     <>
-   <Layout>
-   
-    
-    <div>
-    < div className={loginStyles.imgwrapper}>
-    <Image src='/assets/1.svg'  height={40} width={60} alt="company name logo"/>
-</div>
-      </div>
-      <div className="text">
-      <div className={loginStyles.wrappersp}>
-        <form action="">
-          
-          <div className={loginStyles.topwrapper}>
+      <Layout>
+        <div className={loginStyles.wrapper}>
+          <div className={loginStyles.imgwrapper}>
+            <Image
+              src="/assets/1.svg"
+              height={200}
+              width={400}
+              alt="company name logo"
+            />
           </div>
-          <div className={loginStyles.inputboxs}>
-            <label htmlFor="username">  </label>
-            <input
-              type="text"
-              placeholder="username"
-              />
-          </div>
-          <div className={loginStyles.inputwrapper}>
-          </div>
-          <div className={loginStyles.inputboxs}>
-            <label htmlFor="Password">  </label>
-            <input  type="Password" placeholder= "Password"  />
-          </div>
-          <div className={loginStyles.inputwrapper}>
-          </div>
-          <nav>
-          <Button >Login</Button>
-          <div className="create-forgot">
-          <label htmlFor=""> </label>
-            <Link href ='/sign-up' className={loginStyles.forgotPassword}>Forgot password?</Link>
-            <Link href ='/sign-up'className={loginStyles.createAccount}> Create Account</Link>
-            
-          </div>
-          </nav>
-        </form>
-      </div>
-    </div>
-    </Layout>
-    </>
-  
 
-  )
+          <div className={loginStyles.wrappersp}>
+            <form action="" className={loginStyles.form}>
+              <div className={loginStyles.topwrapper}></div>
+              <div className={loginStyles.inputboxs}>
+                <label htmlFor="username"> </label>
+                <input type="text" placeholder="username" />
+              </div>
+              <div className={loginStyles.inputwrapper}></div>
+              <div className={loginStyles.inputboxs}>
+                <label htmlFor="Password"> </label>
+                <input type="Password" placeholder="Password" />
+              </div>
+              <div className={loginStyles.inputwrapper}></div>
+              <nav>
+                <Button>Login</Button>
+                <div className="create-forgot">
+                  <label htmlFor=""> </label>
+                  <Link href="/sign-up" className={loginStyles.forgotPassword}>
+                    Forgot password?
+                  </Link>
+                  <Link href="/sign-up" className={loginStyles.createAccount}>
+                    {" "}
+                    Create Account
+                  </Link>
+                </div>
+              </nav>
+            </form>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
 }
 
-
-  
 export default LoginPage;
